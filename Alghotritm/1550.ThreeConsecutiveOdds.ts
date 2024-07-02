633ac31d1e7408df23d0d53332f1086cb05714ba
AleZane29 <alessio.zanella29@gmail.com>
2024-07-02 12:13:34 +0200

    Aggiunti 350.ts e 1550.ts

---
+++ Alghotritm/1550.ThreeConsecutiveOdds.ts
@@ -0,0 +1,15 @@
+function threeConsecutiveOdds(arr: number[]): boolean {
+	let consOdd = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 1) {
+			consOdd++;
+
+			if (consOdd >= 3) {
+				return true;
+			}
+		} else {
+			consOdd = 0;
+		}
+	}
+	return false;
+}
