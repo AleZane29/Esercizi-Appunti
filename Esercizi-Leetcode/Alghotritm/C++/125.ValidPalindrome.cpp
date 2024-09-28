c1ff4f375e456ee882fdd9391a4711b20793cc93
AleZane29 <alessio.zanella29@gmail.com>
2024-09-28 12:34:36 +0200

    Aggiunto 67.cpp e sistemato intellisense

--- Esercizi-Leetcode/Alghotritm/C++/125.ValidPalindrome.cpp
+++ Esercizi-Leetcode/Alghotritm/C++/125.ValidPalindrome.cpp
@@ -1,15 +1,28 @@
-class Solution {
+#include <iostream>
+using namespace std;
+
+// Data una stringa s contenente numeri, lettere e altri caratteri mantenere solo numeri e lettere e restituire true se la stringa è
+// palindroma, false altrimenti
+class Solution
+{
 public:
-    bool isPalindrome(string s) {
-        string res = "";
-        for(int i=0; i<s.length(); i++){
-            if(isalnum(s[i])){res+=tolower(s[i]);}
-        }
-        for(int i=0; i<res.length()/2; i++){
-            if(res[i]!=res[res.length()-1-i]){
-                return false;
-            }
-        }
-        return true;
+  bool isPalindrome(string s)
+  {
+    string res = "";
+    for (int i = 0; i < s.length(); i++)
+    {
+      if (isalnum(s[i]))
+      {
+        res += tolower(s[i]);
+      }
     }
+    for (int i = 0; i < res.length() / 2; i++)
+    {
+      if (res[i] != res[res.length() - 1 - i])
+      {
+        return false;
+      }
+    }
+    return true;
+  }
 };
